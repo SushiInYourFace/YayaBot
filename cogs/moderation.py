f5baf7bd5c3aa11bdbd8d34237b9789ff5fc7910
SushiInYourFace <joel_olmsted@icloud.com>
2021-03-05 17:32:38 -0500

    added purge command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,5 +6,14 @@ class Moderation(commands.Cog):
         self.bot = bot
         self._last_member = None
 
+    @commands.command(help="Purges a specified amount of messages from the chat")
+    @commands.has_permissions(manage_messages=True)
+    async def purge(self, ctx, arg):
+        try:
+            arg = int(arg)
+        except ValueError:
+            await ctx.send("That's not a valid number. To use this command, please use the number of messages to purge as your argument")
+        await ctx.channel.purge(limit=arg)
+
 def setup(bot):
     bot.add_cog(Moderation(bot))
