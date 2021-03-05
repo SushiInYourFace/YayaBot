8b16a9eca805c0bf4cb8cf7336015f50df9ff466
SushiInYourFace <joel_olmsted@icloud.com>
2021-03-04 19:45:55 -0500

    hello command

--- cogs/cmty.py
+++ cogs/cmty.py
@@ -6,12 +6,17 @@ class Community(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
+
     #rats
     @commands.command(help="RATS RATS RATS RATS RATS RATS RATS RATS RATS RATS RATS RATS", aliases=["rat", "RATS", "RAT"])
     async def rats(self, ctx):
         rat = random.choice(open("RATSRATSRATS.txt").readlines())
         await ctx.send(rat)
 
+    #hello
+    @commands.command(help="Says hello")
+    async def hello(self, ctx):
+        await ctx.send("Hello there")
 
 def setup(bot):
     bot.add_cog(Community(bot))
