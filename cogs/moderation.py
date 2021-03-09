1367a8beb044b31d801e435511e241966a183363
SushiInYourFace <joel_olmsted@icloud.com>
2021-03-09 15:37:16 -0500

    harsher punishments now overwrite shorter ones

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -259,15 +259,21 @@ class Sql:
     def new_case(self, user, guild, casetype, reason, started, expires, mod):
         caseID = self.newest_case()
         if expires != -1:
+            #checks if user already has an active case of the same type, and removes it if it is less severe
+            unexpired_cases = cursor.execute("SELECT id FROM caselog WHERE guild=? AND user=? AND type=? AND expires >=? AND expires <=? ", (guild,user, casetype, time.time(), expires)).fetchall()
+            #should only ever be <=1 case that meets these criteria, but better safe than sorry
+            if unexpired_cases is not None:    
+                for case in unexpired_cases:
+                    cursor.execute("DELETE FROM active_cases WHERE id = ?", (case[0],))
             cursor.execute("INSERT INTO active_cases(id, expiration) VALUES(?,?)", (caseID, expires))
         cursor.execute("INSERT INTO caselog(id, guild, user, type, reason, started, expires, moderator) VALUES(?,?,?,?,?,?,?,?)", (caseID, guild, user, casetype, reason, started, expires, mod))
         connection.commit()
 
     def get_role(self, guild, role):
         if role == "gravel":
-            roleid = cursor.execute("SELECT gravel from role_ids WHERE guild = ?", (guild,)).fetchone()
+            roleid = cursor.execute("SELECT gravel FROM role_ids WHERE guild = ?", (guild,)).fetchone()
         elif role == "muted":
-            roleid = cursor.execute("SELECT muted from role_ids WHERE guild = ?", (guild,)).fetchone()
+            roleid = cursor.execute("SELECT muted FROM role_ids WHERE guild = ?", (guild,)).fetchone()
         return roleid[0]
 
 SqlCommands = Sql()
