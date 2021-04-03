f935739a8965d55069d25223361811f0835c6ae6
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2021-04-03 21:49:54 +0100

    Worm Command,
    - Created resources/ for cog resources
    - Moved rats.txt to resources//
    - Added worm.png for...
    - Added worm command in community

--- cogs/community.py
+++ cogs/community.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import random
+import io
+from PIL import Image
 
 class Community(commands.Cog):
     """Commands for the community!"""
@@ -11,7 +13,7 @@ class Community(commands.Cog):
     #rats
     @commands.command(help="RATS RATS RATS RATS RATS RATS RATS RATS RATS RATS RATS RATS", aliases=["rats", "RATS", "RAT"])
     async def rat(self, ctx):
-        rat = random.choice(open("rats.txt").readlines())
+        rat = random.choice(open("resources/rats.txt").readlines())
         await ctx.send(rat)
 
     #hello
@@ -27,6 +29,32 @@ class Community(commands.Cog):
     async def on_message(self,message):
         if message.type == discord.MessageType.new_member:
             await message.add_reaction("🎉")
+
+    @commands.command(name="worm",aliases=["wormonastring","woas","string"])
+    async def worm(self,ctx,*,user:discord.Member=None):
+        if not user:
+            user = ctx.author
+        image,wormColour = await self.get_worm(user.id)
+        await ctx.send(f"{user.display_name} is a {discord.Colour.from_rgb(wormColour[0],wormColour[1],wormColour[2])} coloured worm!",file=image)
+
+    async def get_worm(self,id,colour=False):
+        if not colour:
+            rand = random.Random(id)
+            wormColour = (rand.randint(1,255),rand.randint(1,255),rand.randint(1,255),255)
+        else:
+            wormColour = id
+        im = Image.open("resources/worm.png")
+        im = im.convert("RGBA")
+        pixels = im.load()
+        for y in range(im.size[1]):
+            for x in range(im.size[0]):
+                if pixels[x,y] == (255,0,0,255):
+                    pixels[x,y] = wormColour
+        arr = io.BytesIO()
+        im.save(arr, format='PNG')
+        arr.seek(0)
+        return discord.File(arr,filename="worm.png"),wormColour
+
 
 def setup(bot):
     bot.add_cog(Community(bot))
