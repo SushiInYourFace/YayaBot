95c67ebceb316b8732d3b8aa67103b50f9000590
SushiInYourFace <joel_olmsted@icloud.com>
2021-05-04 11:19:56 -0400

    added ping commad

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -318,6 +318,11 @@ class Utilities(commands.Cog):
         connection.commit()
         await ctx.send(f"Tag role set to {role.name}.")
 
+    @commands.command()
+    @commands.check(functions.has_modrole)
+    async def ping(self, ctx):
+        await ctx.send(f"Pong! {round((self.bot.latency*1000),4)} ms")
+
 
 
 def setup(bot):
