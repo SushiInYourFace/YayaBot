69becaf91feedd25799146b2dcacffe9ec9472d2
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2021-06-29 14:23:42 +0100

    Added version check

--- start.py
+++ start.py
@@ -1,9 +1,22 @@
 import logging
 import subprocess
 import sys
-import pkg_resources
 
 logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.INFO)
+
+# IF YOU ARE EDITING THIS: make sure the script is compatable with lower versions of python (this means no fstrings)
+
+if not (sys.version_info[0] >= 3 and sys.version_info[1] >= 8):
+    logging.warning("Python version 3.8+ is required, you are running this with " + ".".join([str(i) for i in sys.version_info[:3]]))
+    exit()
+
+pip = True
+
+try:
+    import pkg_resources
+except:
+    logging.warning("pip is not installed. Continuing without requirements check.")
+    pip = False
 
 run = True
 
@@ -11,17 +24,17 @@ def requirements_check():
     try:
         pkg_resources.require(open('requirements.txt',mode='r'))
     except pkg_resources.DistributionNotFound as error:
-        logging.warning(f"{error}! Run '{sys.executable} -m pip install requirements' as admin/sudo or with '--user'.")
+        logging.warning(str(error)+"! Run '"+sys.executable+" -m pip install -r requirements.txt' as admin/sudo or with '--user'.")
     exit()
 
 while run:
     subprocess.run(["git","fetch","origin"])
     b = subprocess.Popen(["git", "rev-parse", "--abbrev-ref", "HEAD"],stdout=subprocess.PIPE)
     branch = b.communicate()[0].decode().replace("\n","")
-    local = subprocess.Popen(["git", "log", "--name-only", f"origin/{branch}..HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    local = subprocess.Popen(["git", "log", "--name-only", "origin/"+branch+"..HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = local.communicate()
     if not out:
-        incoming = subprocess.Popen(["git", "diff", "--name-only", "HEAD", f"origin/{branch}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        incoming = subprocess.Popen(["git", "diff", "--name-only", "HEAD", "origin/"+branch], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = incoming.communicate()
         if out:
             subprocess.run(["git", "pull"], stdout=subprocess.PIPE)
@@ -30,7 +43,8 @@ while run:
             logging.info("No Update Required.")
     else:
         logging.info("Committed changes, not updating.")
-    requirements_check()
+    if pip:
+        requirements_check()
     code = subprocess.run([sys.executable,"bot.py"],stdout=subprocess.PIPE).returncod
     if code == 0:
         run = False
