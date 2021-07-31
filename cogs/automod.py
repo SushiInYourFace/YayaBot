522d4337ea780340fc51b026425aae32f132e8e8
MrOinky <61294271+MrOinky@users.noreply.github.com>
2021-07-31 23:20:35 +0100

    Fix message edits

--- cogs/automod.py
+++ cogs/automod.py
@@ -499,6 +499,7 @@ class AutoMod(commands.Cog):
         end = []
 
         for i in enumerate(result):
+            i = i[0]
             if result[i].startswith("- "):
                 start.append("~~" + result[i][2:] + "~~")
             elif result[i].startswith("+ "):
@@ -512,6 +513,7 @@ class AutoMod(commands.Cog):
         #formats strikethroughs pretty
         for i in enumerate(start):
             try:
+                i = i[0]
                 if start[i].endswith("~~") and start[i+1].startswith("~~"):
                     start[i] = start[i][:-2]
                     start[i+1] = start[i+1][2:]
